f7ad76d945eb55b78209af1a68617eaf2a9fea3f
Jason Luebke <jjluebke@gmail.com>
2024-01-10 16:28:53 -0500

    Saving work

---
+++ includes/footer.js
@@ -0,0 +1,7 @@
+(() => {
+  const footer = '<section class="content">' +
+    '<div class="note">All rights reserved, copyright Jason Luebke, 2024</div>' +
+  '</section>'
+
+  document.querySelector('footer').appendChild(footer)
+})()
