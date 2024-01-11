e5f31d2e68f483566a1d3ef805629c6f585a434f
Jason Luebke <jjluebke@gmail.com>
2024-01-11 14:46:57 -0500

    Add jekyll

--- includes/footer.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const footer = '<section class="content">' +
-    '<div class="note">All rights reserved, copyright Jason Luebke, 2024</div>' +
-  '</section>'
-
-  document.querySelector('footer').appendChild(footer)
-})()
